612e0dc9a95b118fa1f5eaf4dcd0185bc5edcdc6
idkngrid <ingrid.camargopr@gmail.com>
2022-12-28 09:44:29 -0300

    Add entitite's logic and tests

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,4 +1,6 @@
 /* eslint-disable prettier/prettier */
+import { randomUUID } from "node:crypto";
+import { Replace } from "src/helpers/Replace";
 import { Content } from "./content";
 
 export interface NotificationProps {
@@ -10,10 +12,19 @@ export interface NotificationProps {
 }
 
 export class Notification {
+    private _id: string;
     private props: NotificationProps;
 
-    constructor(props: NotificationProps) {
-        this.props = props;
+    constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+        this._id = randomUUID();
+        this.props = {
+            ...props,
+            createdAt: props.createdAt ?? new Date()
+        };
+    }
+
+    public get id(): string {
+        return this._id;
     }
 
     public set recipientId(recipientId: string) {
