6181dc708c314a517fa7875fc25421525d9715f9
idkngrid <ingrid.camargopr@gmail.com>
2022-12-30 14:57:42 -0300

    Add all use cases to the providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,3 +1,8 @@
+import { CancelNotification } from "@app/use-cases/cancel-notification";
+import { CountRecipientNotifications } from "@app/use-cases/count-recipient-notifications";
+import { GetRecipientNotifications } from "@app/use-cases/get-recipient-notifications";
+import { ReadNotification } from "@app/use-cases/read-notification";
+import { UnreadNotification } from "@app/use-cases/unread-notification";
 import { Module } from "@nestjs/common";
 import { SendNotification } from "src/app/use-cases/send-notification";
 import { DatabaseModule } from "../database/database.module";
@@ -6,7 +11,14 @@ import { NotificationsController } from "./controllers/notifications.controller"
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationsController],
-    providers: [SendNotification],
+    providers: [
+        SendNotification,
+        GetRecipientNotifications,
+        CountRecipientNotifications,
+        CancelNotification,
+        ReadNotification,
+        UnreadNotification
+    ],
 })
 
 export class HttpModule {}
